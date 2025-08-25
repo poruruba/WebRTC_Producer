d2647c91c5940feac56e0374237c6db9ee474073
poruruba <44470781+poruruba@users.noreply.github.com>
2025-08-25 17:11:01 +0900

    Add files via upload

---
+++ api/controllers/signaling-ws/index.js
@@ -0,0 +1,256 @@
+'use strict';
+
+const HELPER_BASE = process.env.HELPER_BASE || "/opt/";
+const Response = require(HELPER_BASE + 'response');
+//const Redirect = require(HELPER_BASE + 'redirect');
+
+const WEBRTC_PING_MESSAGE = "9ca3b441-9558-4ba2-afbf-ebd518ecdc03";
+const WEBRTC_PONG_MESSAGE = "9ca3b442-9558-4ba2-afbf-ebd518ecdc03";
+const WEBRTC_PING_INTERVAL = 25000;
+
+let channel_list = [];
+let ping_interval_id = 0;
+
+exports.handler = async (event, context, callback) => {
+	// console.log(event);
+  if (event.path == "/signaling-get-all"){
+    // チャネルリストの返却
+    var list = channel_list.map(item =>{
+      return {
+        channelId: item.channelId,
+        clients: item.clients
+      };
+    });
+    return new Response({ channel_list: list } );
+  }else
+  
+  if( event.path == "/signaling-get-client"){
+    // クライアントリストの返却
+    var item = channel_list.find(item => item.channelId == event.queryStringParameters["channelId"] )
+    var list = item ? item.clients : [];
+    return new Response({ client_list: list });
+  }else
+  
+  {
+    console.log("unknown endpoint");
+    throw new Error("unknown endpoint");
+  }
+};
+
+exports.ws_handler = async (event, context) => {
+  // Websocketの接続維持
+  if( event.body == WEBRTC_PING_MESSAGE ){
+    await context.wslib.postToConnection({
+      ConnectionId: event.requestContext.connectionId,
+      Data: WEBRTC_PONG_MESSAGE
+    }, null);
+    return { statusCode: 200 };
+  }else if( event.body == WEBRTC_PONG_MESSAGE ){
+    return { statusCode: 200 };
+  }
+
+//  console.log(event);
+//  console.log(context);
+  console.log(event.body);
+
+  try{
+    var body = JSON.parse(event.body);
+
+    // チャネルの検索
+    var channel_item = channel_list.find(item => item.channelId == body.channelId );
+    if( !channel_item ){
+      channel_item = {
+        channelId: body.channelId,
+        password: body.password,
+        clients: [],
+      };
+      channel_list.push(channel_item);
+    }
+
+    // クライアントの検索
+    var client_item;
+    if( body.type == "ready" ){
+      // クライアントからの接続
+      // パスワードのチェック
+      if( channel_item.password != body.password ){
+        console.log("invalid password");
+        await context.wslib.postToConnection({
+          ConnectionId: event.requestContext.connectionId,
+          Data: JSON.stringify({
+            type: "error",
+            clientId: body.clientId,
+            message: "invalid password"
+          })
+        }, null);
+        return { statusCode: 200 };
+      }
+      // クライアントの再登録
+      var index = channel_item.clients.findIndex(item => item.clientId == body.clientId );
+      if( index >= 0 ){
+        channel_item.clients.splice(index, 1);
+      }
+      client_item = {
+        role: body.role,
+        connectionId: event.requestContext.connectionId,
+        clientId: body.clientId,
+        state: "ready"
+      };
+      channel_item.clients.push(client_item);
+    }else{
+      // クライアントの検索
+      client_item = channel_item.clients.find(item => item.clientId == body.clientId);
+      if( !client_item ){
+        console.log("unknown clientId");
+        throw new Error("unknown clientId");
+      }
+      // コネクションIDのチェック
+      if( client_item.connectionId != event.requestContext.connectionId ){
+        console.log("invalid connection");
+        throw new Error("invalid connection");
+      }
+    }
+
+    // 応答処理
+    if( body.type == "ready"){
+      // for クライアントからの接続
+      if( client_item.role == "master" ){
+        // masterの場合：各クライアントにreadyを返却
+        for( let item of channel_item.clients ){
+          if( item.clientId == client_item.clientId || item.role != 'slave')
+            continue;
+          await context.wslib.postToConnection({
+            ConnectionId: item.connectionId,
+            Data: JSON.stringify({
+              type: "ready",
+              clientId: client_item.clientId,
+              clients: [{
+                role: client_item.role,
+                clientId: client_item.clientId,
+                state: client_item.state
+              }]
+            })
+          }, null);
+        }
+      }else
+      if( client_item.role == "slave" ){
+        // slaveの場合： 現在コネクションの返却
+        var clients = [];
+        for( let item of channel_item.clients ){
+          if( item.clientId == client_item.clientId || item.role != 'master' )
+            continue;
+          clients.push({
+            role: item.role,
+            clientId: item.clientId,
+            state: item.state
+          });
+        }
+        await context.wslib.postToConnection({
+          ConnectionId: client_item.connectionId,
+          Data: JSON.stringify({
+            type: "ready",
+            clientId: client_item.clientId,
+            clients: clients
+          })
+        }, null);
+      }else
+      if( client_item.role == "direct" ){
+        // directの場合： 現在コネクションの返却
+        var clients = [];
+        for( let item of channel_item.clients ){
+          if( item.clientId == client_item.clientId || item.role != 'direct' )
+            continue;
+          clients.push({
+            role: item.role,
+            clientId: item.clientId,
+            state: item.state
+          });
+        }
+        await context.wslib.postToConnection({
+          ConnectionId: client_item.connectionId,
+          Data: JSON.stringify({
+            type: "ready",
+            clientId: client_item.clientId,
+            clients: clients
+          })
+        }, null);
+      }      
+    }else
+    if( body.type == "sdpOffer1" || body.type == "sdpOffer2" || body.type == "sdpAnswer" || body.type == "iceCandidate" ){
+      // 通信内容をターゲットクライアントに転送
+      var item = channel_item.clients.find(item => item.clientId == body.target );
+      if( item ){
+        await context.wslib.postToConnection({
+          ConnectionId: item.connectionId,
+          Data: JSON.stringify({
+            type: body.type,
+            clientId: client_item.clientId,
+            data: body.data
+          })
+        }, null);
+        if( body.type == "sdpOffer1" || body.type == "sdpOffer2") client_item.state = "offering";
+        else if( body.type == "sdpAnswer" ) item.state = "answered";
+      }
+    } 
+
+    return { statusCode: 200 };
+  }catch(error){
+    console.error(error);
+  }
+};
+
+function loop_ping(wslib){
+  wslib.getConnectionList((err, list) =>{
+    if( err )
+      return;
+    for( let item of list){
+      wslib.postToConnection({
+        ConnectionId: item,
+        Data: WEBRTC_PING_MESSAGE
+      }, null);
+    }
+  });
+}
+
+exports.connect_handler = async (event, context) => {
+  console.log("connect_handler");
+
+  // 接続維持の開始
+  if( ping_interval_id == 0 ){
+    ping_interval_id = setInterval(() =>{
+      loop_ping(context.wslib);
+    }, WEBRTC_PING_INTERVAL );
+  }
+
+  return { statusCode: 200 };
+};
+
+exports.disconnect_handler = async (event, context) => {
+  console.log("disconnect_handler");
+
+  // チャネルリストの更新
+  var list = [];
+  for( let channel of channel_list ){
+    var clients = [];
+    for( let client of channel.clients ){
+      if( client.connectionId != event.requestContext.connectionId )
+        clients.push(client);
+    }
+    if( clients.length > 0 ){
+      channel.clients = clients;
+      list.push(channel);
+    }
+  }
+  channel_list = list;
+
+  context.wslib.getConnectionList((err, list) =>{
+    if( err )
+      return;
+    if( list.length == 0 ){
+      // 接続維持の停止
+      clearInterval(ping_interval_id);
+      ping_interval_id = 0;
+    }
+  });
+
+  return { statusCode: 200 };
+};
---
+++ public/js/start.js
@@ -0,0 +1,263 @@
+'use strict';
+
+//const vConsole = new VConsole();
+//const remoteConsole = new RemoteConsole("http://[remote server]/logio-post");
+//window.datgui = new dat.GUI();
+
+const SIGNALING_URL = "wss://【立ち上げたサーバのホスト名】/signaling";
+const WEBRTC_CHANNEL_ID = "WebRTC_Channel";
+const WEBRTC_CLIENT_ID = "WebRTC_Client";
+const WEBRTC_CHANNEL_PASSWORD = "password";
+const BACKGROUND_IMAGE_URL = "./image/rectangle_large_type_2_1b5a3cb412dffc38339fd16cc9290b19.png";
+
+var g_webrtcDirect = null;
+var g_stream = null;
+
+var vue_options = {
+    el: "#top",
+    mixins: [mixins_bootstrap],
+    store: vue_store,
+    router: vue_router,
+    data: {
+        remoteClientId: null,
+        signalingUrl: SIGNALING_URL,
+        config: {},
+        facing_mode: "environment",
+        is_webrtc: false,
+        is_signaling: false,
+        message: "",
+        received_datetime: 0,
+        received_message: "",
+        received_remoteClientId: "",
+    },
+    computed: {
+    },
+    methods: {
+        show_url_qrcode: function(){
+            var qrcode = new QRCode(document.querySelector("#qrcode"), {
+                text: location.href,
+                correctLevel: QRCode.CorrectLevel.H
+            });
+            this.dialog_open('#qrcode_dialog');
+        },
+        show_qrcode: function(){
+            var qrcode = new QRCode(document.querySelector("#qrcode"), {
+                text: this.received_message,
+                correctLevel: QRCode.CorrectLevel.H
+            });
+            this.dialog_open('#qrcode_dialog');
+        },
+
+        stop_webrtc: async function(){
+            await g_webrtcDirect.stop();
+            this.is_webrtc = false;
+        },
+
+        start_webrtc: async function () {
+            try {
+                await g_webrtcDirect.start(this.config);
+                localStorage.setItem("webrtc_config", JSON.stringify(this.config));
+                this.is_signaling = true;
+            } catch (error) {
+                console.error(error);
+                this.toast_show(error);
+            }
+        },
+
+        attach_default: async function(){
+            g_webrtcDirect.replaceTrack(null);
+
+            const video = document.querySelector('#localcamera_view');
+            video.src = null;
+            video.srcObject = null;
+            this.video_ended = false;
+        },
+        attach_display: async function(){
+            try{
+                if( !navigator.mediaDevices || navigator.mediaDevices.getDisplayMedia )
+                    throw new Error("not display stream");
+
+                var stream = await navigator.mediaDevices.getDisplayMedia({ video: true, audio: true });
+                const video = document.querySelector('#localcamera_view');
+                video.src = null;
+                video.srcObject = stream;
+                this.video_ended = false;
+
+                g_webrtcDirect.replaceTrack(stream);
+            }catch(error){
+                console.error(error);
+                alert(error);
+            }
+        },
+        attach_camera: async function(){
+            try{
+                if( !navigator.mediaDevices || !navigator.mediaDevices.getUserMedia )
+                    throw new Error("not media stream");
+
+                const constraints = {
+                    video: { facingMode: this.facing_mode },
+                    audio: { echoCancellation: true, noiseSuppression: true },
+                };
+                var stream = await navigator.mediaDevices.getUserMedia(constraints);
+                const video = document.querySelector('#localcamera_view');
+                video.src = null;
+                video.srcObject = stream;
+                this.video_ended = false;
+
+                g_webrtcDirect.replaceTrack(stream);
+            }catch(error){
+                console.error(error);
+                alert(error);
+            }
+        },
+
+        change_videoFile: async function(files){
+            if( files.length == 0 )
+                return;
+
+            try{
+                var file = files[0];
+                const video = document.querySelector('#localcamera_view');
+                const url = URL.createObjectURL(file);
+                video.srcObject = null;
+                video.src = url;
+                this.video_ended = true;
+                await video.play();
+            }catch(error){
+                console.error(error);
+                alert(error);
+            }
+        },
+
+        video_playing: async function(){
+            if( this.video_ended ){
+                this.video_ended = false;
+                const video = document.querySelector('#localcamera_view');
+                const stream = video.captureStream();
+                g_webrtcDirect.replaceTrack(stream);
+            }
+        },
+
+        video_end: async function(){
+            this.video_ended = true;
+        },
+
+        send_file: async function(files){
+            if( files.length == 0 )
+                return;
+
+            var file = files[0];
+            var buffer = await file.arrayBuffer();
+            g_webrtcDirect.sendBinary(buffer, file.name, file.type);
+        },
+
+        send_message: async function(){
+            await g_webrtcDirect.sendMessage(this.message);
+            this.message = "";
+        },
+
+        goto_fullscreen() {
+            const video = document.querySelector('#remotecamera_view');
+            if (video.requestFullscreen) {
+                video.requestFullscreen();
+            } else if (video.webkitRequestFullscreen) {
+                video.webkitRequestFullscreen(); // Safari
+            } else if (video.msRequestFullscreen) {
+                video.msRequestFullscreen(); // IE/Edge
+            }
+        },
+
+        onWebrtcCallback: function(module, result){
+            console.log(module, result);
+            if (module == "peer") {
+                if (result.type == "sdpOffering1" || result.type == "sdpOffering2" ) {
+                    var remoteView = document.querySelector('#remotecamera_view');
+                    remoteView.srcObject = null;
+                    remoteView.src = null;
+                    g_stream = new MediaStream();
+                } else
+                if (result.type == "track") {
+                    if (result.kind == "audio" || result.kind == "video") {
+                        if( result.track ){
+                            var remoteView = document.querySelector('#remotecamera_view');
+                            g_stream.addTrack(result.track);
+                            remoteView.srcObject = g_stream;
+                        }
+                    }
+                }else
+                if( result.type == "connectionstatechange") {
+                    if( result.connectionState == "connected"){
+                        this.remoteClientId = g_webrtcDirect.remoteClientId;
+                        this.is_webrtc = true;
+                        this.toast_show("WebRTCが接続されました。");
+                    }
+                    if( result.connectionState == "disconnected"){
+                        this.is_webrtc = false;
+                        this.remoteClientId = null;
+                        g_webrtcDirect.stopStream(g_stream);
+                        this.toast_show("WebRTCが切断されました。");
+                    }
+                }
+            } else if (module == "signaling") {
+                if (result.type == "ready") {
+                    if( result.remoteClientList.length > 0)
+                        g_webrtcDirect.connect(result.remoteClientList[0].clientId);
+                }else if( result.type == "closed" ){
+                    this.toast_show("Signalingが切断されました。");
+                    this.is_signaling = false;
+                }else if( result.type == "error"){
+                    this.toast_show(result.message);
+                    this.is_signaling = false;
+                }
+            }else if( module == "data" ){
+                var info = result.info;
+                if( info.type == "message"){
+                    this.received_datetime = new Date().getTime();
+                    this.received_message = info.message;
+                    this.received_remoteClientId = result.remoteClientId;
+                    this.toast_show(info.message, result.remoteClientId);
+                    console.log(`message:[${result.remoteClientId}] ${info.message}`);
+                }else
+                if( info.type == "binary"){
+                    if( !info.array ){
+                        this.toast_show("ファイル受信中...");
+                        return;
+                    }
+                    var blob = new Blob([info.array], { type: info.mime_type });
+                    var url = window.URL.createObjectURL(blob);
+                    var a = document.createElement("a");
+                    a.href = url;
+                    a.target = '_blank';
+                    a.download = info.fname;
+                    a.click();
+                    window.URL.revokeObjectURL(url);
+                }
+            }
+        },
+    },
+    created: function(){
+    },
+    mounted: async function(){
+        proc_load();
+
+        var config = localStorage.getItem("webrtc_config");
+        if( config ){
+            this.config = JSON.parse(config);
+        }else{
+            this.config = {
+                channelId: WEBRTC_CHANNEL_ID,
+                clientId: WEBRTC_CLIENT_ID,
+                password: WEBRTC_CHANNEL_PASSWORD,
+            };
+        }
+
+        g_webrtcDirect = new WebrtcDirect(this.signalingUrl, this.onWebrtcCallback, BACKGROUND_IMAGE_URL);
+    }
+};
+vue_add_data(vue_options, { progress_title: '' }); // for progress-dialog
+vue_add_global_components(components_bootstrap);
+vue_add_global_components(components_utils);
+
+/* add additional components */
+  
+window.vue = new Vue( vue_options );
